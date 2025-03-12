9864e17626656a5957884f7a303b294970866188
niqlli <154437179+niqlli@users.noreply.github.com>
2025-03-12 17:55:59 +0500

    Add files via upload

---
+++ laba3/lab3.3/lab3.3/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let rec getDirectory (text: string) =
+    printf "%s" text
+    let input = Console.ReadLine()
+    if Directory.Exists(input) then
+        input
+    else
+        printfn "Ошибка (каталог не существует). Пожалуйста, введите корректный путь."
+        getDirectory text
+
+let directoryPath = getDirectory "Введите путь к каталогу: "
+
+let rec getFiles directoryPath =
+    seq {
+        yield! Directory.EnumerateFiles(directoryPath) 
+    }
+
+let files = lazy (getFiles directoryPath)  
+
+let lastFile = 
+    files.Value
+    |> Seq.sort 
+    |> Seq.tryLast 
+
+match lastFile with
+| Some file -> printfn "Последний файл по алфавиту: %s" file
+| None -> printfn "В каталоге нет файлов."
